b87f69b4c68fe4f3b87f7941d5877fb79d6be6fd
Mohan Praneeth Rajulapudi <45649126+rajulapudi@users.noreply.github.com>
2019-08-23 20:53:05 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Narasimha' });
+  res.render('index', { title: 'Prashanth' });
 });
 
 module.exports = router;
