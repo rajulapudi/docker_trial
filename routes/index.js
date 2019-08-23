1f4313f5b696bac348fbb69267ac7e5d7e0efef2
Mohan Praneeth Rajulapudi <45649126+rajulapudi@users.noreply.github.com>
2019-08-23 21:28:15 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'THS6' });
+  res.render('index', { title: 'THS7' });
 });
 
 module.exports = router;
